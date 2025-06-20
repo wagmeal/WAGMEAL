bff504c69f3d7646f5a6cfc8cdc6f16dfed579a2
Takumi Kowatari <kowatari19960531@gmail.com>
2025-06-20 20:59:25 +0900

    Initial Commit

---
+++ Dogfood/DogfoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogfoodApp.swift
+//  Dogfood
+//
+//  Created by takumi kowatari on 2025/06/20.
+//
+
+import SwiftUI
+
+@main
+struct DogfoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
